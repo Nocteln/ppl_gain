701758e9e6943d7d1a4571fd3b720b92c66c0cb6
eliott <eliottmieze@gmail.com>
2022-05-31 18:15:42 +0200

    pleins de trucs

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ new ResizeObserver(entries => {
     navLinksContainer.style.transition = "none"
   }
 }).observe(document.body)
+
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
